41f2599a59b5c64c9d21e34d0dd6adc270b7ecfa
ivanes285 <ivanes285@gmail.com>
2023-01-29 20:16:41 -0500

    Test

--- server/main.go
+++ server/main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"log"
+	"os"
 )
 
 func main() {
-	app := fiber.New()                // Creamos una instancia de la aplicación de Fiber
-	app.Static("/", "../client/dist") // Establecemos los archivos estáticos para el frontend en este caso desde (React)
+	app := fiber.New()            // Creamos una instancia de la aplicación de Fiber
+	app.Static("/", "../dist")    // Establecemos los archivos estáticos para el frontend en este caso desde (React)
 
-	app.Use(cors.New(cors.Config{    // Configuración de CORS para permitir el acceso a la API desde cualquier origen
+	app.Use(cors.New(cors.Config{ // Configuración de CORS para permitir el acceso a la API desde cualquier origen
 		AllowOrigins: "*",
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
@@ -38,10 +38,10 @@ func main() {
 		})
 	})
 
-	PORT:= os.Getenv("PORT")                      // Obtenemos el puerto de la variable de entorno PORT
+	PORT := os.Getenv("PORT") // Obtenemos el puerto de la variable de entorno PORT
 	if PORT == "" {
-	  PORT = "3000"
+		PORT = "3000"
 	}
-	log.Fatal(app.Listen(":"+ PORT))      // Iniciamos el servidor y si hay un error lo mostramos en la consola
+	log.Fatal(app.Listen(":" + PORT)) // Iniciamos el servidor y si hay un error lo mostramos en la consola
 	fmt.Println("Server is running on port", PORT)
 }
