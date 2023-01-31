615fb7cfda96f08d4cf1e40049b7459ec9f3d923
ivanes285 <ivanes285@gmail.com>
2023-01-31 16:59:06 -0500

    Add settings to server

--- server/main.go
+++ server/main.go
@@ -1,82 +1,63 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/ivanes285/GOReactMongo/db"
+	"github.com/ivanes285/GOReactMongo/routes"
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"log"
-	"os" 
-	"github.com/ivanes285/GOReactMongo/models"  // forma de importar un paquete local (models) tome en cuenta la ruta del paquete (models) 
 )
 
 func main() {
-	app := fiber.New()                // Creamos una instancia de la aplicación de Fiber
-	app.Static("/", "../client/dist") // Establecemos los archivos estáticos para el frontend en este caso desde (React)
 
-	//SETTING CORS
+	err := run()    // ejecutamos la función run y la asignamos a la variable err
+	if err != nil { // si err es diferente de nil entonces mostramos el error en la consola
+		panic(err)
+	}
+}
+
+func run() error {
+
+	// VALIDATION .ENV
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found")
+	}
+
+	// DATABASE
+	err = db.ConnectDB() // Conectamos a la base de datos
+	if err != nil {
+		return err
+	}
+
+	defer db.CloseDB() // Defer permite ejecutar una función al final en este caso para cerrar la conexión a la base de datos
+
+	// SERVER
+	app := fiber.New() // Creamos una instancia de la librería Fiber para crear el servidor
+
+	// MIDDLEWARES
+	app.Use(logger.New())         // logger permite mostrar en la consola las peticiones que se hacen a la API
+	app.Use(recover.New())        // recover permite mostrar en la consola los errores y no se caiga el servidor en el caso que se ejecute un panic
 	app.Use(cors.New(cors.Config{ // Configuración de CORS para permitir el acceso a la API desde cualquier origen
 		AllowOrigins: "*",
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
 
-	// VALIDATION .ENV
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
-	}
-
-	// CONNECT TO MONGODB  (ATLAS)
-	MONGOURI := os.Getenv("MONGODB_URI")
-	if MONGOURI == "" {
-		log.Fatal("Debes establecer una variable de entorno llamada 'MONGODB_URI' para establecer la conección")
-	}
-	//context es un objeto que se utiliza para cancelar operaciones o para establecer un tiempo de espera
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(MONGOURI))
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Connected to MongoDB")
-
-	// defer permite ejecutar una función al finalizar el programa (en este caso la desconexión de la base de datos)
-	defer func() {
-		if err := client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-		fmt.Println("Connection to MongoDB closed.")
-	}() // () para ejecutar la función
-
-	coll := client.Database("goreactmongo").Collection("users")
-
-	// INSERTAR UN DOCUMENTO
-	coll.InsertOne(context.TODO(), bson.M{"name": "John"})
+	// ROUTES
+     routes.AddUsersGroup(app)   // Agregamos las rutas de los usuarios
 
 
-
-	//ROUTES
-	app.Get("/api/v1/users", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
-			"message": "Get users",
-		})
-	})
-
-	app.Post("/api/v1/users", func(c *fiber.Ctx) error {
-
-		var user models.User
-		if err := c.BodyParser(&user); err != nil {
-			return err
-		}
-	      
-		return c.JSON(fiber.Map{
-			"data": "Guardando user",
-		})
-	})
+	// STATIC FILES (REACT)
+	app.Static("/", "../client/dist") // Establecemos los archivos estáticos para el frontend en este caso desde (React)
 
 
-	
+	// PORT
 	PORT := os.Getenv("PORT") // Obtenemos el puerto de la variable de entorno PORT
 	if PORT == "" {
 		PORT = "4000"
@@ -85,4 +66,5 @@ func main() {
 	//SERVER
 	log.Fatal(app.Listen(":" + PORT)) // Iniciamos el servidor y si hay un error lo mostramos en la consola
 	fmt.Println("Server is running on port", PORT)
+	return nil // retornamos nil porque no hay error
 }
