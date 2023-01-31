debad9330aeb2a3c0a4dfa9a5872526a029dc1b2
ivanes285 <ivanes285@gmail.com>
2023-01-31 15:32:03 -0500

    Add model user

--- server/models/user.go
+++ server/models/user.go
@@ -1,6 +1,7 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive" //para usar el tipo ObjectID
 type User struct {
-  Name  string `json:"name"`
+	ID   primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"` //bson es para mongo
+	Name string `json:"name"`
 }
-
