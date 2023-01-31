0f8226a6ed13f8727a32018c28e6c506b14c4aa9
ivanes285 <ivanes285@gmail.com>
2023-01-30 23:13:03 -0500

    Add Environment Variables

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,28 @@
-
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  const fechatData = async () => {
-    const response = await fetch("http://127.0.0.1:3000/api/v1/users");
-    const data = await response.json();
-   console.log(data);
+
+  const [name, setName] = useState<string>("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const response = await axios.post(import.meta.env.VITE_URLSERVER+"/users"); //usando variable de entorno en vite
+    console.log(response.data);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
   };
 
   return (
     <div>
-      <h1>Hello World</h1>
-      <button onClick={() => fechatData()}>Obtener Datos</button>
+      <form onSubmit={handleSubmit }>
+        <input type="text" name="name" placeholder="Ingresa tu nombre" onChange={handleChange} />
+        <button>Guardar</button>
+      </form>
     </div>
   );
-  
 }
 
-export default App
+export default App;
